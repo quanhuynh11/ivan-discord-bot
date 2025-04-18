b7f2e024bac75a2c5c7ecabe22a99ea7f3216e55
Quan Huynh <152340025+quanhuynh11@users.noreply.github.com>
2025-04-18 03:40:53 -0600

    add random ak character

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('messageCreate', async (message) => {
     if (message.content === '!ping') {
         // Respond with "Pong!", and response time
 
-        // Reponse time starts here
+        // Response time starts here
         const responseTime = Date.now();
 
         // Respond
@@ -49,6 +49,63 @@ client.on('messageCreate', async (message) => {
         const data = await response.json();
         // console.log(data);
         message.reply(`Quote: "${data.data.content}" \nAnime: -${data.data.anime.name}-`);
+    }
+
+    // Command: !randomAK
+    // return a random arknights character 
+    // params: star 1-6
+    // example: !randomAK star 5
+    if (message.content.startsWith('!randomAK')) {
+        // Remove the command prefix and split the rest by comma
+        const args = message.content.slice('!randomAK'.length).trim().split(',');
+
+        // default
+        let star = null;
+
+        const tags = [];
+
+        args.forEach((arg) => {
+            const trimmedArg = arg.trim();
+            const starMatch = trimmedArg.match(/^star\s+(\d+)$/i);
+
+            if (starMatch) {
+                star = parseInt(starMatch[1]);
+            }
+            else if (trimmedArg) {
+                tags.push(trimmedArg);
+            }
+        });
+
+        try {
+            const response = await fetch(`https://api.rhodesapi.com/api/search?rarity=${star}`);
+
+            if (!response.ok) {
+                message.reply('Invalid arguments. Usage: !randomAK [star] [tag1, tag2, ...], or it broke.');
+                throw new Error('Failed to fetch character data.');
+            }
+
+            const data = await response.json();
+
+            // Get a random character by index
+            const randomCharacter = data[Math.floor(Math.random() * data.length)];
+    
+            const embed = {
+                title: randomCharacter.name,
+                description: randomCharacter.biography,
+            };
+    
+            // console.log(embed);
+            // console.log(star);
+            // console.log(tags);
+    
+            message.channel.send({ embeds: [embed] });
+
+        }
+        catch (error) {
+            console.error(error);
+            return;
+        }
+
     }
 });
 
